c312a9cd1344bd0d306ad70fd3a788b9727dba8c
Seçkin Aktunç <95690007+seckinaktunc@users.noreply.github.com>
2026-04-05 06:34:15 +0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,10 @@
-export default function Button({ label, onClick }: { label: string, onClick?: () => void }) {
+interface ButtonProps {
+    label: string;
+    onClick?: () => void;
+    disabled?: boolean;
+}
+
+export default function Button({ label, onClick }: ButtonProps) {
     return (
         <div
             className="html-div xdj266r x14z9mp xat24cr x1lziwak xexx8yu xyri2b x18d9i69 x1c1uobl x9f619 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv xuk3077 x1oa3qoh x1nhvcw1"
