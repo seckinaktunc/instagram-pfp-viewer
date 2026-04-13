eaebc023cbf1c4a6e3424015775cda5f404ea58f
Seçkin Aktunç <95690007+seckinaktunc@users.noreply.github.com>
2026-04-14 01:50:50 +0300

    Refactor miscellaneous components

---
+++ src/components/miscellaneous/Spinner.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Spinner() {
+    return (
+        <div
+            className="html-div x14z9mp x1lziwak xexx8yu xyri2b x18d9i69 x1c1uobl x9f619 x82snj4 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw xdj266r xat24cr x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh xl56j7k"
+            style={{ "--x-minHeight": "40px" } as React.CSSProperties}
+        >
+            <div
+                className="x78zum5 xdt5ytf xl56j7k x1nrll8i x10l6tqk xwa60dl x11lhmoz"
+                data-visualcompletion="loading-state"
+                role="progressbar"
+                style={{ height: "32px", width: "32px" }}
+            >
+                <svg
+                    aria-label="Loading..."
+                    className="xemfg65 xa4qsjk x1ka1v4i xbv57ra"
+                    role="img"
+                    viewBox="0 0 100 100"
+                >
+                    <rect className="x1i210e2" height="6" opacity="0" rx="3" ry="3" transform="rotate(-90 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.08333333333333333" rx="3" ry="3" transform="rotate(-60 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.16666666666666666" rx="3" ry="3" transform="rotate(-30 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.25" rx="3" ry="3" transform="rotate(0 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.3333333333333333" rx="3" ry="3" transform="rotate(30 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.4166666666666667" rx="3" ry="3" transform="rotate(60 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.5" rx="3" ry="3" transform="rotate(90 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.5833333333333334" rx="3" ry="3" transform="rotate(120 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.6666666666666666" rx="3" ry="3" transform="rotate(150 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.75" rx="3" ry="3" transform="rotate(180 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.8333333333333334" rx="3" ry="3" transform="rotate(210 50 50)" width="25" x="72" y="47" />
+                    <rect className="x1i210e2" height="6" opacity="0.9166666666666666" rx="3" ry="3" transform="rotate(240 50 50)" width="25" x="72" y="47" />
+                </svg>
+            </div>
+        </div>
+    );
+}
