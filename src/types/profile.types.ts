817605e3afa3bd757eaaac022685734651ab5b17
Seçkin Aktunç <95690007+seckinaktunc@users.noreply.github.com>
2026-04-14 01:55:36 +0300

    Add hasStory boolean

--- src/types/profile.types.ts
+++ src/types/profile.types.ts
@@ -1,10 +1,11 @@
 export interface ProfileData {
     url: string | null;
-    username: string | null;
+    username: string;
     isVerified: boolean;
     biography: string;
     fullName: string;
     postCount: number;
     followerCount: number;
     followingCount: number;
+    hasStory: boolean;
 }
